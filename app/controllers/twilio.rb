645de4f4bf7f37ea9e1b99a450542e4a13704694
Krista Starr <email": null>
2017-08-18 12:20:22 -0500

    changed twilio controller name

--- app/controllers/twilio.rb
+++ app/controllers/twilio.rb
@@ -1,6 +1,6 @@
 require 'twilio-ruby'
 
-class TwilioController < ApplicationController
+class TwiliosController < ApplicationController
 # require 'rubygems'
 
 
@@ -10,23 +10,23 @@ end
 
 
 def send_text_message
-  account_sid = 'AC95dba1864d9daff62e736a02d0402d21'
-  auth_token = '1e34eae13b33504dbe5b50c9a0c51c97'
-  twilio_phone_number = '5128317223'
+  # account_sid = 'AC95dba1864d9daff62e736a02d0402d21'
+  # auth_token = '1e34eae13b33504dbe5b50c9a0c51c97'
+  # twilio_phone_number = '5128317223'
 
-  number_to_send_to = params[:number_to_send_to]
-  message = params[:message]
+  # number_to_send_to = params[:number_to_send_to]
+  # message = params[:message]
 
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  # @client = Twilio::REST::Client.new account_sid, auth_token
 
-  @client.messages.create({
-   from: "+1#{twilio_phone_number}",
-   to: "+1#{number_to_send_to}",
-   body: message
-   })
-  end
+  # @client.messages.create({
+  #  from: "+1#{twilio_phone_number}",
+  #  to: "+1#{number_to_send_to}",
+  #  body: message
+  #  })
+  # end
 
-  redirect_to '/twilio/index'
+  # redirect_to '/twilio/index'
 
 end
 
