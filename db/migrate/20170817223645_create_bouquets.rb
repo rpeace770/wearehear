aef71d2c2606afb9d4d655a428fafc84c195c8cd
Rachel Peace <email": null>
2017-08-18 10:03:36 -0500

    Add associations

--- db/migrate/20170817223645_create_bouquets.rb
+++ db/migrate/20170817223645_create_bouquets.rb
@@ -3,7 +3,7 @@ class CreateBouquets < ActiveRecord::Migration[5.1]
     create_table :bouquets do |t|
       t.integer :sender_id
       t.integer :receiver_id
-      t.integer :track_id
+      t.integer :song_id
       t.string :custom_location
       t.integer :location_id
       t.datetime :expiration_date
