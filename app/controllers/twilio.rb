5616675e811655b61222f2226e7ab42ad4a5ed3b
Krista Starr <email": null>
2017-08-18 09:47:00 -0500

    add twilio controllr

---
+++ app/controllers/twilio.rb
@@ -0,0 +1,40 @@
+require 'twilio-ruby'
+
+class TwilioController < ApplicationController
+# require 'rubygems'
+
+
+def index
+
+end
+
+
+def send_text_message
+  account_sid = 'AC95dba1864d9daff62e736a02d0402d21'
+  auth_token = '1e34eae13b33504dbe5b50c9a0c51c97'
+  twilio_phone_number = '5128317223'
+
+  number_to_send_to = params[:number_to_send_to]
+  message = params[:message]
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
+ @client.messages.create({
+   from: "+1#{twilio_phone_number}",
+   to: "+1{number_to_send_to}",
+   body: message
+   })
+  end
+
+  redirect_to '/twilio/index'
+
+end
+
+
+# @client = Twilio::REST::Client.new account_sid, auth_token
+
+#  @client.messages.create({
+#   body: "hello from my rails app",
+#    to: "19038144150",
+#    from: "15128317223"
+#    })
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get '/twilio/index' => 'twilio#index'
+post '/twilio/send_text_message'  => 'twilio#send_text_message'
+
+root 'twilio#index'
+
 end
