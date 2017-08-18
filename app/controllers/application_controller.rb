4837e41e7cd19ca9bffcbfc8f0350a45e89ebf52
Rachel Peace <email": null>
2017-08-18 12:33:12 -0500

    Add devise to project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # added from omniauth article, can change if we need to
+  before_action :authenticate_user!
 end
