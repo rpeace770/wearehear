536f9a0ee042720985e084c64f6e0b34a0807f1e
Rachel Peace <email": null>
2017-08-21 14:15:05 -0500

    Finish heat map

--- app/assets/javascripts/heatmap.js
+++ app/assets/javascripts/heatmap.js
@@ -1,66 +1,62 @@
-// var mapbase, heatmap;
+$(document).ready(initHeatMap);
 
-// function initHeatMap() {
-//      mapbase = new google.maps.Map(document.getElementById('mapbase'), {
-//           zoom: 13,
-//           center: {lat: 30.267, lng: -97.743}
-//      });
+var mapbase, heatmap;
 
-// NO NOT WORKING
-     // function get_data() {
-     //      var get_it = getPoints();
-     //      get_it.done(function(response) {
-     //           var array = [];
-     //           response.forEach(function(location) {
-     //                array.push(new google.maps.LatLng(location.lat, location.lng));
-     //           });
-     //           return array;
-     //      });
-     // }
-// NO NOT WORKING
-//      heatmap = new google.maps.visualization.HeatmapLayer({
-//           data: get_data(),
-//           map: mapbase
-//      });
-// }
+function initHeatMap() {
+     mapbase = new google.maps.Map(document.getElementById('mapbase'), {
+          zoom: 4,
+          center: {lat: 40.267, lng: -97.743}
+     });
+          var get_it = getPoints();
+          var here = get_it.done(function(response) {
+               var array = [];
+               response.forEach(function(location) {
+                    array.push(new google.maps.LatLng(location.lat, location.lng));
+               });
+               heatmap = new google.maps.visualization.HeatmapLayer({
+                    data: array,
+                    map: mapbase
+                });
+          });
+}
 
-// function toggleHeatmap() {
-//      heatmap.setMap(heatmap.getMap() ? null : mapbase);
-// }
+function toggleHeatmap() {
+     heatmap.setMap(heatmap.getMap() ? null : mapbase);
+}
 
-// function changeGradient() {
-//      var gradient = [
-//      'rgba(0, 255, 255, 0)',
-//      'rgba(0, 255, 255, 1)',
-//      'rgba(0, 191, 255, 1)',
-//      'rgba(0, 127, 255, 1)',
-//      'rgba(0, 63, 255, 1)',
-//      'rgba(0, 0, 255, 1)',
-//      'rgba(0, 0, 223, 1)',
-//      'rgba(0, 0, 191, 1)',
-//      'rgba(0, 0, 159, 1)',
-//      'rgba(0, 0, 127, 1)',
-//      'rgba(63, 0, 91, 1)',
-//      'rgba(127, 0, 63, 1)',
-//      'rgba(191, 0, 31, 1)',
-//      'rgba(255, 0, 0, 1)'
-//      ];
-//      heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-// }
+function changeGradient() {
+     var gradient = [
+     'rgba(0, 255, 255, 0)',
+     'rgba(0, 255, 255, 1)',
+     'rgba(0, 191, 255, 1)',
+     'rgba(0, 127, 255, 1)',
+     'rgba(0, 63, 255, 1)',
+     'rgba(0, 0, 255, 1)',
+     'rgba(0, 0, 223, 1)',
+     'rgba(0, 0, 191, 1)',
+     'rgba(0, 0, 159, 1)',
+     'rgba(0, 0, 127, 1)',
+     'rgba(63, 0, 91, 1)',
+     'rgba(127, 0, 63, 1)',
+     'rgba(191, 0, 31, 1)',
+     'rgba(255, 0, 0, 1)'
+     ];
+     heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+}
 
-// function changeRadius() {
-//      heatmap.set('radius', heatmap.get('radius') ? null : 20);
-// }
+function changeRadius() {
+     heatmap.set('radius', heatmap.get('radius') ? null : 20);
+}
 
-// function changeOpacity() {
-//      heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-// }
+function changeOpacity() {
+     heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+}
 
-// // Heatmap data: 500 Points
-// function getPoints() {
-//      var search = $.ajax({
-//           url: 'bouquets/all',
-//           method: "post"
-//      });
-//      return search;
-// }
+// Heatmap data: 500 Points
+function getPoints() {
+     var search = $.ajax({
+          url: 'bouquets/all',
+          method: "post"
+     });
+     return search;
+}
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find(params[:id])
-    if current_user.id != user.id
+    @user = User.find(params[:id])
+    if current_user.id != @user.id
       redirect_to "/"
     end
   end
