26a6a6c1f2faf891e9422b09d761aef867fbef5e
Katie McNeil <katie.mcneil@live.com>
2017-08-20 19:51:49 -0500

    Continue modifying show page and user creation process.

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -5,7 +5,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     @user.picture_url = request.env["omniauth.auth"].info.images.first["url"]
     @user.username = request.env["omniauth.auth"].info.display_name
-    binding.pry
     sign_in_and_redirect @user
   end
 end
