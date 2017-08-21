9e3f3644f9f838b9cc8142eeddebd69f4730e6e5
Rachel Peace <email": null>
2017-08-21 10:38:02 -0500

    Save float numbers and bouquet show page is fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820173206) do
+ActiveRecord::Schema.define(version: 20170821153324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20170820173206) do
   end
 
   create_table "locations", force: :cascade do |t|
-    t.integer "lat"
-    t.integer "lng"
+    t.float "lat"
+    t.float "lng"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
