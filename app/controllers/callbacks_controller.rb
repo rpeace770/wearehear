9d62821f682bd4c94a5f19612c778d3da6e68886
Sinclair Swan <email": null>
2017-08-22 11:34:23 -0500

    fixed user creation error

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,8 +3,8 @@ require 'pry'
 class CallbacksController < Devise::OmniauthCallbacksController
   def spotify
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if request.env["omniauth.auth"].info.images.length > 0
-      @user.picture_url = request.env["omniauth.auth"].info.images.first["url"]
+    if request.env["omniauth.auth"].info.image.length > 0
+      @user.picture_url = request.env["omniauth.auth"].info.image
     end
     @user.username = request.env["omniauth.auth"].info.display_name
     sign_in_and_redirect @user
