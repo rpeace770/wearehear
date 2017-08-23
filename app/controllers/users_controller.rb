951b12d1f65b159079a2538b06b2276dfc5607e9
Rachel Peace <email": null>
2017-08-23 15:28:06 -0500

    Add player on user page which refreshes with any new songs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
       client_token = Base64.strict_encode64("7a4280c69bd540d588a6540f043ffa48:b07dd2e7bab84c498c5e804b64a267eb")
       spotify_token = RestClient.post("https://accounts.spotify.com/api/token",{"grant_type": "client_credentials"}, {"Authorization": "Basic #{client_token}"})
       parsed_token = JSON.parse(spotify_token)
-      # binding.pry
     if @user.playlist == nil
       playlist = RestClient.post("https://api.spotify.com/v1/users/#{current_user.uid}/playlists",{name: 'New Thing' }.to_json, {"Authorization": "Bearer #{current_user.token}","Content-Type": "application/json"})
       parse_playlist = JSON.parse(playlist)
@@ -20,14 +19,14 @@ class UsersController < ApplicationController
       @user.save
     end
     if @user.sent_bouquets
-    tracks_array = []
-    current_user.sent_bouquets.each do |bouquet|
-      song = Song.find(bouquet.song_id)
-      tracks_array << "spotify:track:#{song.spotify_id}"
+      tracks_array = []
+      current_user.sent_bouquets.each do |bouquet|
+        song = Song.find(bouquet.song_id)
+        tracks_array << "spotify:track:#{song.spotify_id}"
+      tracks = RestClient.put("https://api.spotify.com/v1/users/#{current_user.uid}/playlists/#{current_user.playlist}/tracks", {"uris": tracks_array}.to_json, {"Authorization": "Bearer #{current_user.token}","Content-Type": "application/json"})
+      parsed_tracks = JSON.parse(tracks)
+      @playlist_tracks_id = parsed_tracks["snapshot_id"]
     end
-    tracks = RestClient.post("https://api.spotify.com/v1/users/#{current_user.uid}/playlists/#{current_user.playlist}/tracks", {"uris": tracks_array}.to_json, {"Authorization": "Bearer #{current_user.token}","Content-Type": "application/json"})
-    parsed_tracks = JSON.parse(tracks)
-    @playlist_tracks_id = parsed_tracks["snapshot_id"]
     end
   end
 
